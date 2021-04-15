9a373ca497460c429261939fa242c6717018293a
smweddell <70972767+smweddell@users.noreply.github.com>
2021-04-15 16:57:33 -0400

    added recipe error class

--- view/recipe/error/recipe_error.class.php
+++ view/recipe/error/recipe_error.class.php
@@ -6,7 +6,36 @@
  * Description:
  */
 
-class recipe_error
+class RecipeError extends RecipeIndexView
 {
+
+    public function display($message) {
+
+        //display page header
+        parent::displayHeader("Error");
+        ?>
+
+        <div id="main-header">Error</div>
+        <hr>
+        <table style="width: 100%; border: none">
+            <tr>
+                <td style="vertical-align: middle; text-align: center; width:100px">
+                    <img src='<?= BASE_URL ?>/www/img/error.jpg' style="width: 80px; border: none"/>
+                </td>
+                <td style="text-align: left; vertical-align: top;">
+                    <h3> Sorry, but an error has occurred.</h3>
+                    <div style="color: red">
+                        <?= urldecode($message) ?>
+                    </div>
+                    <br>
+                </td>
+            </tr>
+        </table>
+        <br><br><br><br><hr>
+        <a href="<?= BASE_URL ?>/recipe/index">Back to recipe list</a>
+        <?php
+        //display page footer
+        parent::displayFooter();
+    }
 
 }
