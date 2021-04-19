b48048b90ea93509a492bf42d1ff2432df09483f
hicksjc <78804634+hicksjc@users.noreply.github.com>
2021-04-18 21:33:38 -0400

    Updated welcome index
    Coded the welcome index file, basically the home page.

--- view/welcome/welcome_index.class.php
+++ view/welcome/welcome_index.class.php
@@ -1,12 +1,51 @@
 <?php
 /**
- * Author: Sydney Weddell
- * Date: 4/15/2021
+ * Author: James Hicks
+ * Date: 4/18/2021
  * File: welcome_index.class.php
- * Description:
+ * Description: Welcome index
  */
 
-class welcome_index
+class WelcomeIndex extends IndexView
 {
+  public function display(){
+        //display page header
+        parent::displyHeader();
+        ?>
+        <section class="home">
+            <div class="image-title">
+                <h1>Savory Foods</h1>
+            </div>
 
+            <div class="categories">
+                <div class="category">
+                    <div class="box appetizers">
+                        <div class="image"><img class="image2" src="<?= BASE_URL ?>/img/appetizers.jpg" alt="appetizers"></div>
+                        <div class="title">Appetizers</div>
+                        <div class="learn-button">Learn More</div>
+                    </div>
+                    <div class="box breakfast">
+                        <div class="image"><img src="<?= BASE_URL ?>/img/breakfast.jpg" alt="breakfast" class="image2"></div>
+                        <div class="title">Breakfast</div>
+                        <div class="learn-button">Learn More</div>
+                    </div>
+                    <div class="box entrees">
+                        <div class="image"><img src="<?= BASE_URL ?>/img/entrees.jpg" alt="Entree" class="image2"></div>
+                        <div class="title">Entrees</div>
+                        <div class="learn-button">Learn More</div>
+                    </div>
+                    <div class="box desserts">
+                        <div class="image"><img src="<?= BASE_URL ?>/img/dessert.jpg" alt="Dessert" class="image2"></div>
+                        <div class="title">Desserts</div>
+                        <div class="learn-button">Learn More</div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+    <?php
+        //display footer
+        parent::displayFooter();
+    }
 }
